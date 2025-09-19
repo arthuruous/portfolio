8b5fda7f9bc253df0f3e60c888d38fb14d32b01d
Arthur Oliveira <arthursateles1@gmail.com>
2025-09-19 03:00:07 +0000

    re fresh

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+      <h1 class="text-3xl font-bold">    Hello world!  </h1>
     </>
   )
 }
